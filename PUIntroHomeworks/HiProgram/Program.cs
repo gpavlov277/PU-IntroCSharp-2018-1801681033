2af16a83b0a21c7b621db4bbeb51495c1b492803
gpavlov277 <gpavlov277@gmail.com>
2018-10-09 15:33:54 +0300

    Included thing from week1 and week2 because of isssue with globalsettings

---
+++ PUIntroHomeworks/HiProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HiProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello I am Georgi Pavlov !");
+        }
+    }
+}
---
+++ PUIntroHomeworks/MyUniqueNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyUniqueNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = Math.PI;
+            Console.WriteLine("How old are you?");
+            int age = int.Parse(Console.ReadLine());
+            int ageAfter10 = age + 10;
+            Console.WriteLine("Enter last 4 digits of your faculty number here: ");
+            int facultyNumber = int.Parse(Console.ReadLine());
+            double result = ((age + 10) * facultyNumber) / pi;
+            Console.WriteLine("After 10 yeras you will be " + ageAfter10 + " yeras old. And your unique number is " +result);
+
+
+        }
+    }
+}
