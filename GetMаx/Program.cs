6978195ee0584771853cc9894ca10bf1a1fe0ef1
gpavlov277 <gpavlov277@gmail.com>
2018-11-08 22:04:00 +0200

    Included homework for week 6.

---
+++ GetMаx/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GetMаx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                
+                Console.WriteLine("Enter 3 numbers.");
+                Console.Write("Enter the first number: ");
+                int firstNumber = int.Parse(Console.ReadLine());
+                Console.Write("Enter the second number: ");
+                int secondNumber = int.Parse(Console.ReadLine());
+                Console.Write("Enter the third number: ");
+                int thirdNumber = int.Parse(Console.ReadLine());
+
+                
+                Console.WriteLine("The biggest number is: " + GetMax(firstNumber, secondNumber, thirdNumber));
+            }
+
+           
+       
+
+       int GetMax(int num1, int num2, int num3)
+        {
+                if (num1 > num2 && num1 > num3)
+                {
+                    return num1;
+                }
+                else if (num2 > num1 && num2 > num3)
+                {
+                    return num2;
+                }
+                else return num3;
+            }
+    }
+}
+    }
+
---
+++ StudentsNamesSum/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace StudentsNamesSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+            
+
+                string[] lines = File.ReadAllLines(@"D:\PU 2018-2019\PU-IntroCSharp-2018-1801681033\PUIntroHomeworks\files\students.txt");
+
+                System.Console.WriteLine("Contents of students.txt = ");
+                foreach (string line in lines)
+                {
+                    string[] currentElements = line.Split(' ');
+
+
+                    string firstName = currentElements[1];
+                   string secondName = currentElements[2];
+                    string lastName = currentElements[3];
+                    string fullName = $"{firstName} {secondName} {lastName}";
+                    int sum = 0;
+
+
+
+
+                for (int i = 0; i < firstName.Length; i++)
+                {
+
+                    sum = sum + (int)firstName[i];
+
+                }
+                for (int i = 0; i < secondName.Length; i++)
+                {
+
+                    sum = sum + (int)secondName[i];
+
+                }
+                for (int i = 0; i < lastName.Length; i++)
+                {
+
+                    sum = sum + (int)lastName[i];
+
+                }
+                if (sum > 15000)
+                {
+
+                    Console.WriteLine($":) {fullName}->{sum}");
+                }
+            }
+            
+
+        }
+        }
+    }
+
+
+   
